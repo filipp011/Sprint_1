ed5cfac9f58e0badf178cf740a581e7063eb2816
Filipp Nis <f.nis61@gmail.com>
2025-04-11 08:03:56 +0300

    Исправил добавление task_005 в completed_tasks и удалил ее из new_tasks в одну строчку методами append и remove через запятую

--- task_4.py
+++ task_4.py
@@ -3,7 +3,7 @@ new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
 completed_tasks = ['task_002', 'task_012', 'task_006']
 
 # 1. Переносим задачу task_005 из new_tasks в completed_tasks
-new_tasks.remove('task_005') and completed_tasks.append('task_005') if 'task_005' in new_tasks else None
+completed_tasks.append('task_005'), new_tasks.remove('task_005')
 
 # 2. Удаляем задачу task_007 из new_tasks
 if 'task_007' in new_tasks:
